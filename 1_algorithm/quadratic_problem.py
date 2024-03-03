c75f9f17fab410a74bb4d543e6676c72c2185370
prachy <prachya@amposgroup.com>
2024-03-03 09:47:19 +0700

    submit the problem

---
+++ 1_algorithm/quadratic_problem.py
@@ -0,0 +1,55 @@
+import math
+
+
+def is_prime(n):
+    # 2 is the smallest n
+    # 1 is the factor of all n
+    # n is the factor of itself
+    for i in range(2, n):
+        if (n % i) == 0:
+            return False
+    return True
+
+
+def find_n_prime(a, b):
+    i = 0
+    running_n = []
+    running_n_prims = []
+    while True:
+        _prime = abs(int(math.pow(i, 2) + (i * a) + b))
+        if is_prime(_prime):  # TODO: can save as cache
+            running_n.append(i)
+            running_n_prims.append(_prime)
+            i = i + 1
+        else:
+            break  # not consecutive prime ever
+
+    # print("a and b: {} | {}".format(a, b))
+    # print("n and length of primes: {} | {}".format((n - 1), list_primes))  # last n is n - 1 that create consecutive
+    return running_n, running_n_prims
+
+
+# time bound: ~ 2000 * 2000 = 4000000
+def main():
+    max_a, max_b = 0, 0
+    max_n, max_primes_n = [], []
+    for a in range(-999, 1000):  # TODO: can optimize to O(N) ?
+        for b in range(-1000, 1001):
+            n, n_primes = find_n_prime(a, b)
+            if len(n) > len(max_n):
+                max_n = n
+                max_primes_n = n_primes
+                max_a = a
+                max_b = b
+
+    # print("---")
+    # print(f"a|b: {max_a}|{max_b} and max_a_dot_b: {max_a * max_b}")
+    # print("---")
+    return max_n, max_primes_n, (max_a * max_b)
+
+
+if __name__ == '__main__':
+    n, primes, dot_ab = main()
+    print(f"max_n: {n}")
+    print(f"max_con_primes: {primes}")
+    print(f"max product a*b: {dot_ab}")
